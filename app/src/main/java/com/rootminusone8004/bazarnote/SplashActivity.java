b299c7cb3d05fca6825869511799949b1d124e0e
rootminusone8004 <rootovertwo8004@tutanota.com>
2024-08-10 22:57:44 +0600

    dark mode in splash screen ensured

--- app/src/main/java/com/rootminusone8004/bazarnote/SplashActivity.java
+++ app/src/main/java/com/rootminusone8004/bazarnote/SplashActivity.java
@@ -2,10 +2,12 @@ package com.rootminusone8004.bazarnote;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,18 +15,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-        Thread thread = new Thread(() -> {
-            try {
-                Thread.sleep(4000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
+        new Handler().postDelayed(() -> {
             Intent intent = new Intent(SplashActivity.this, SessionActivity.class);
             startActivity(intent);
             finish();
-        });
-        thread.start();
+        }, 2000);
     }
 }
